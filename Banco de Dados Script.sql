e69bcbd9520e5962c24e4704f053b0e776834dac
Bruno <bruno.tita@sptech.school>
2026-04-28 13:52:00 -0300

    Banco de Dados Script.sql

--- Banco de Dados Script.sql
+++ Banco de Dados Script.sql
@@ -2,8 +2,8 @@ create database Projeto_Individual;
 
 use Projeto_Individual;
 
-Create Table Usuário(
-ID_usuario Int Primary Key Not Null Auto_Increment,
+Create Table Usuario(
+ID_usuario Int Primary Key Auto_Increment,
 Nome Varchar(100) Not Null,
 Email Varchar(100) Not Null Unique,
 Telefone Char(14) Not Null Unique,
@@ -11,7 +11,7 @@ CPF Char(11) Not Null Unique,
 Senha Varchar(100) Not Null
 ) Auto_Increment = 1000;
 
-Create Table Endereço(
+Create Table Endereco(
 Estado Varchar(100) Not Null,
 Cidade Varchar(100) Not Null,
 Bairro Varchar(100) Not Null,
@@ -25,7 +25,8 @@ Nome Varchar(100) Not Null,
 Classificao Varchar(20) Not Null,
 Descricao_Da_Classificacao Varchar(100) Not Null,
 Qualidade Varchar(100) Not Null,
-Descricao Varchar(700) Not Null,
-FK_Carta Int Primary Key Not Null,
-Foreign Key (FK_Carta) References Usuário (ID_Usuario)
+Descricao Varchar(300) Not Null,
+FK_Carta Int Not Null,
+Primary Key (FK_Carta, ID_Carta),
+Foreign Key (FK_Carta) References Usuario (ID_Usuario)
 );

36a509732831156a2175d6226515767c30d38acd
BrunoHenrique78 <bruno.tita@sptech.school>
2026-05-31 20:49:10 -0300

    ultima atualização

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -9,14 +9,16 @@ comandos para mysql server
 create database pokeswap;
 use pokeswap;
 
-CREATE TABLE usuario (
+CREATE TABLE usuario(
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(100) NOT NULL
+    senha VARCHAR(100) NOT NULL,
+    pais VARCHAR(50),
+	estado VARCHAR(50)
 ) 	AUTO_INCREMENT = 1000;
 
-CREATE TABLE cartas_salvas (
+CREATE TABLE cartas_salvas(
     id_carta INT PRIMARY KEY AUTO_INCREMENT,
     fk_usuario_id INT,
     carta_id VARCHAR(50),
@@ -26,5 +28,26 @@ CREATE TABLE cartas_salvas (
     carta_raridade VARCHAR(50),
     carta_imagem VARCHAR(255),
     data_adicao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    descricao TEXT,
     FOREIGN KEY (fk_usuario_id) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE forum_mensagens(
+    id_mensagem INT PRIMARY KEY AUTO_INCREMENT,
+    fk_carta INT,
+    fk_usuario INT,
+    mensagem VARCHAR(500),
+    data_msg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (fk_carta) REFERENCES cartas_salvas(id_carta),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE cartas_gerais(
+    id_publicacao INT PRIMARY KEY AUTO_INCREMENT,
+    fk_carta INT,
+    fk_usuario INT,
+    descricao TEXT,
+    data_publicacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (fk_carta) REFERENCES cartas_salvas(id_carta),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
 );
